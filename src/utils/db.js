cb3453af02857c08115e3c7d8ba39b49fceff7de
Oskar Rough <oskar@rough.dk>
2019-01-05 21:37:01 +0100

    Refactor to use R4 sdk

--- src/utils/db.js
+++
@@ -1,3 +0,0 @@
-const toObject = (obj, id) => Object.assign(obj, {id})
-export const toArray = data => Object.keys(data).map(id => toObject(data[id], id))
-export const endpoint = 'https://radio4000.firebaseio.com'
