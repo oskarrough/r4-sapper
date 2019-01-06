7c4750b1aa91c1d38721dbd2c3524f6e80e58dca
Oskar Rough <oskar@rough.dk>
2019-01-06 14:47:54 +0100

    Add editorconfig

--- src/client.js
+++ src/client.js
@@ -2,9 +2,9 @@ import * as sapper from '../__sapper__/client.js'
 import {Store} from 'svelte/store.js'
 
 sapper.start({
-  target: document.querySelector('#sapper'),
-  store: data => {
-    // 'data' is whatever was in the server-side store.
-    return new Store(data)
-  }
+	target: document.querySelector('#sapper'),
+	store: data => {
+		// 'data' is whatever was in the server-side store.
+		return new Store(data)
+	}
 })
--- src/server.js
+++ src/server.js
@@ -8,17 +8,17 @@ const {PORT, NODE_ENV} = process.env
 const dev = NODE_ENV === 'development'
 
 polka() // You can also use Express
-  .use(
-    compression({threshold: 0}),
-    sirv('static', {dev}),
-    sapper.middleware({
-      store: request => {
-        return new Store({
-          slug: ''
-        })
-      }
-    })
-  )
-  .listen(PORT, err => {
-    if (err) console.log('error', err)
-  })
+	.use(
+		compression({threshold: 0}),
+		sirv('static', {dev}),
+		sapper.middleware({
+			store: request => {
+				return new Store({
+					slug: ''
+				})
+			}
+		})
+	)
+	.listen(PORT, err => {
+		if (err) console.log('error', err)
+	})
